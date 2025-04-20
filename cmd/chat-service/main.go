b2aa4e7dadcb6a36b3fec586208f12d8662cdea8
Никишин Данил Дмитриевич <dndev@mail.ru>
2025-04-20 22:41:44 +0300

    faet(mod-03): ref integration

--- cmd/chat-service/main.go
+++ cmd/chat-service/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"errors"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
-	"context"
 
 	application "github.com/dndev-xx/go-ninja-chat/pkg/context"
-	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -24,18 +25,17 @@ func main() {
 		GetContext()
 
 	if err != nil {
-		fmt.Printf("Failed to build app: %v\n", err)
+		log.Fatalf("Failed to build app: %v\n", err)
 		os.Exit(1)
 	}
-	
-	go func(){ if err := app.DebugServer.Run(ctx); err != nil {
-		app.Logger.Error("Error running server", zap.String("server", "debug"), zap.String("err", err.Error()))
-	}}()
-	go func(){ if err := app.ClientServer.Run(ctx); err != nil {
-		app.Logger.Error("Error running server", zap.String("server", "client"), zap.String("err", err.Error()))
-	}}()
-	
-	<-ctx.Done()
-	fmt.Println("Shutting down gracefully...")
-}
 
+	eg, ctx := errgroup.WithContext(ctx)
+
+	eg.Go(func() error { return app.DebugServer.Run(ctx) })
+	eg.Go(func() error { return app.ClientServer.Run(ctx) })
+
+	if err = eg.Wait(); err != nil && !errors.Is(err, context.Canceled) {
+		log.Fatalf("run app: %v", err)
+	}
+	log.Println("Shutting down gracefully...")
+}
