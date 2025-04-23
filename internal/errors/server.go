8ec3d46a0176e042332d3c2bf7981a4343feebe9
Никишин Данил Дмитриевич <dndev@mail.ru>
2025-04-24 02:25:31 +0300

    feat(mod-03): impl errhandler

---
+++ internal/errors/server.go
@@ -0,0 +1,65 @@
+package errors
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+const (
+	defaultMsg = "something went wrong"
+)
+
+// ServerError is used to return custom error codes to client.
+type ServerError struct {
+	Code    int
+	Message string
+	cause   error
+}
+
+func NewServerError(code int, msg string, err error) *ServerError {
+	return &ServerError{
+		Code:    code,
+		Message: msg,
+		cause:   err,
+	}
+}
+
+func (s *ServerError) Error() string {
+	if s.cause != nil {
+		return fmt.Sprintf("%s: %v", s.Message, s.cause)
+	}
+	return s.Message
+}
+
+func (s *ServerError) Unwrap() error {
+	return s.cause
+}
+
+func GetServerErrorCode(err error) int {
+	code, _, _ := ProcessServerError(err)
+	return code
+}
+
+// ProcessServerError tries to retrieve from given error its code, message and some details.
+// For example, that fields can be used to build error response for client.
+func ProcessServerError(err error) (code int, msg string, details string) {
+    if err == nil {
+        return http.StatusOK, "", ""
+    }
+
+    var serverErr *ServerError
+    if errors.As(err, &serverErr) {
+        return serverErr.Code, serverErr.Message, serverErr.Error()
+    }
+
+    var httpErr *echo.HTTPError
+    if errors.As(err, &httpErr) {
+        msg := fmt.Sprint(httpErr.Message)
+        return httpErr.Code, msg, fmt.Sprintf("code=%d, message=%s", httpErr.Code, msg)
+    }
+
+    return http.StatusInternalServerError, "something went wrong", err.Error()
+}
---
+++ internal/server-client/errhandler/errhandler.go
@@ -0,0 +1,48 @@
+package errhandler
+
+import (
+	"net/http"
+
+	internalerrors "github.com/dndev-xx/go-ninja-chat/internal/errors"
+	"github.com/labstack/echo/v4"
+	"go.uber.org/zap"
+)
+
+var _ echo.HTTPErrorHandler = Handler{}.Handle
+
+//go:generate options-gen -out-filename=errhandler_options.gen.go -from-struct=Options
+type Options struct {
+	logger          *zap.Logger                                    `option:"mandatory" validate:"required"`
+	productionMode  bool                                           `option:"mandatory"`
+	responseBuilder func(code int, msg string, details string) any `option:"mandatory" validate:"required"`
+}
+
+type Handler struct {
+	lg              *zap.Logger
+	productionMode  bool
+	responseBuilder func(code int, msg string, details string) any
+}
+
+func New(opts Options) (Handler, error) {
+	if err := opts.Validate(); err != nil {
+		return Handler{}, err
+	}
+	return Handler{
+		lg: opts.logger,
+		productionMode: opts.productionMode,
+		responseBuilder: opts.responseBuilder,
+	}, nil
+}
+
+func (h Handler) Handle(err error, eCtx echo.Context) {
+	code, msg, details := internalerrors.ProcessServerError(err)
+	httpStatus := http.StatusOK
+	responseDetails := ""
+	if !h.productionMode {
+		responseDetails = details
+	}
+	response := h.responseBuilder(code, msg, responseDetails)
+	if err := eCtx.JSON(httpStatus, response); err != nil {
+		h.lg.Error("failed to send error response", zap.Error(err))
+	}
+}
